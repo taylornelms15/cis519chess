8612936f4ceb6ae7eb8f109561e40fa41f9af8a1
taylornelms15 <taylornelms15@gmail.com>
2020-04-20 12:55:26 -0400

    initial file commit for PGN ingester

---
+++ PGNIngest.py
@@ -0,0 +1,25 @@
+"""
+@file   PGNIngest.py
+@author Taylor Nelms
+
+Scripts to take in some PGN games and transform them into
+ML-compatible game state representations
+"""
+
+import logging
+from log import setupLogging
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    setupLogging()
+    main()
