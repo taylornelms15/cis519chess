1a96c3fe213fc04c1ee0c2e0550d9b064806487c
Harshmeet Singh <53871103+harshmeet2701@users.noreply.github.com>
2020-05-02 12:27:41 -0400

    Adding a dummy file which will be filled with functions to evaluate the trained model by supervised and self-play

---
+++ result.py
@@ -0,0 +1,15 @@
+"""
+@file result.py
+
+Checking how the model is going to perform.
+"""
+
+# Initialization
+
+
+def __init__(self):
+    self.model = self.load_model
+
+
+# Function to load the trained Model
+def load_model(self):
