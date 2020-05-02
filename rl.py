0932b415b85bde452e55ed8b8ab0d844c9525ac8
Harshmeet Singh <53871103+harshmeet2701@users.noreply.github.com>
2020-05-02 11:41:46 -0400

    Starting with the RL code

---
+++ rl.py
@@ -0,0 +1,15 @@
+"""
+@file   RL.py
+
+Self play of the model
+"""
+
+# Initialization
+
+
+def __init__(self):
+    self.model = self.load_model
+
+
+# Function to load the trained Model
+def load_model(self):
