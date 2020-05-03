1a1131ce13bc6003b70a332e16a7fd98ef01ee6c
taylornelms15 <taylornelms15@gmail.com>
2020-05-03 12:57:18 -0400

    annotations and altered program flow for the SupervisedChess object

--- Supervised.py
+++ Supervised.py
@@ -34,17 +34,38 @@ class ArgsFaker(object):
     parameters into our parsing/training functions, without having to go through command line parsing
     """ 
     
-    def __init__(self, pgnFiles = None, tensorData = None, outModel = None, outDict = None, outTensor = None):
+    def __init__(self, pgnFiles = None, tensorData = None, outModel = None, outTensor = None):
         self.pgnFiles   = pgnFiles
         self.tensorData = tensorData
         self.outModel   = outModel
-        self.outDict    = outDict
         self.outTensor  = outTensor
 
 class SupervisedChess(object):
     __slots__ = [ "model" ]
 
-    def __init__(self, savedModel = None, pgnTensors = None, pgnFiles = None, outTensor = None, outModel = None):
+    def __init__(self, savedModel = None):
+        """
+        Initializes the SupervisedChess object, with an optional "already trained model" torch file
+        :param savedModel:      Filepath (or open File object) for the pre-trained pytorch model
+        """
+        if savedModel != None:
+            try:
+                self.model = torch.load(savedModel)
+            except Exception as e:
+                logging.error("Bad input %s to SupervisedChess; need file-like or path name for torch to load" % savedModel)
+                raise
+        else:
+            model = ChessNet()
+
+    def trainModel(self, pgnFiles = None, pgnTensors = None, outTensor = None, outModel = None):
+        """
+        Actually trains the SupervisedChess object
+        :param pgnFiles     : Python List of filenames or open file objects that are valid pgn files.
+        :param pgnTensors   : Singular filename or open file object that is a stored data tensor object
+        :param outTensor    : Optional filename or open file object to which to write the parsed pgn moves
+        :param outModel     : Optional filename or open file object to which to save the trained model object
+        """
+        #previously: def __init__(self, savedModel = None, pgnTensors = None, pgnFiles = None, outTensor = None, outModel = None):
         if pgnFiles != None:
             pgnFiles = [open(x, "r") if isinstance(x, str) else x for x in pgnFiles]
             args = ArgsFaker(pgnFiles = pgnFiles, outTensor = outTensor, outModel = outModel)
@@ -53,12 +74,6 @@ class SupervisedChess(object):
             pgnTensors = open(pgnTensors, "rb") if isinstance(pgnTensors, str) else pgnTensors
             args = ArgsFaker(tensorData = pgnTensors, outTensor = outTensor, outModel = outModel)
             self.model = self._modelFromPgnOrTensors(args)
-        elif savedModel != None:
-            try:
-                self.model = torch.load(savedModel)
-            except Exception as e:
-                logging.error("Bad input %s to SupervisedChess; need file-like or path name for torch to load" % savedModel)
-                raise
         else:
             raise ValueError("Cannot make a SupervisedChess object with no input data")
 
@@ -66,12 +81,11 @@ class SupervisedChess(object):
         if outModel != None:
             torch.save(self.model, outModel) 
     
-    @classmethod
-    def _modelFromPgnOrTensors(cls, args):
+    def _modelFromPgnOrTensors(self, args):
         logging.info("Loading training data:")
         dataset = getTensorData(args)
         logging.info("Training data loaded")
-        model = trainNetworkOnData(dataset)
+        model = trainNetworkOnData(dataset, self.model)
         return model
 
     @classmethod
@@ -293,19 +307,17 @@ def getTensorData(args):
 
         if (args.pgnFiles):
             states, moves = getStateMoveArrays(args)
-        elif (args.pickledFile):
-            inTuple = pickle.load(args.pickledFile)
-            states = inTuple[0]
-            moves = inTuple[1]
+        else:
+            raise ValueError("Need pgnFiles on args object")
 
-        if args.outPickle != None:
-            outTuple = (states, moves)
-            pickle.dump(outTuple, args.outPickle)
+        #if args.outPickle != None:
+        #    outTuple = (states, moves)
+        #    pickle.dump(outTuple, args.outPickle)
 
-        probDict = stateMoveArraysToProbDict(states, moves)
+        #probDict = stateMoveArraysToProbDict(states, moves)
         
-        if args.outDict != None:
-            pickle.dump(probDict, args.outDict)
+        #if args.outDict != None:
+        #    pickle.dump(probDict, args.outDict)
 
         states = torch.stack([gameStateToTensor(x) for x in states])
         moves  = torch.stack([moveToTensor(x)      for x in moves])
@@ -318,13 +330,14 @@ def getTensorData(args):
         return torch.load(args.tensorData)
         #TODO: do something to load the dataset from file here
 
-def trainNetworkOnData(dataset, learn_rate = 0.001):
+def trainNetworkOnData(dataset, model = None, learn_rate = 0.001):
     """
     Takes in our TensorDataset, trains a ChessNet on it
     """
     torch.manual_seed(10)#for reproducability or something
-
-    model = ChessNet()
+    
+    if model == None:
+        model = ChessNet()
     #Potential expansion: putting things onto the GPU for training (relevant if on computer with GPU, which I am not)
     logging.info("Starting ChessNet Training:")
 
@@ -351,48 +364,30 @@ Main, command line parsing
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-P", "--outPickle", help="File to which to write out picked parsed data",
+    parser.add_argument("-T", "--outTensor", help="File to which to write out loaded and parsed data",
                         type=argparse.FileType("wb"), nargs="?")
-    parser.add_argument("-o", "--outTensor", help="File to which to write out loaded and parsed data",
+    parser.add_argument("-M", "--outModel", help="File to which to write out loaded, parsed, and trained model",
                         type=argparse.FileType("wb"), nargs="?")
-    parser.add_argument("-m", "--outModel", help="File to which to write out loaded, parsed, and trained model",
-                        type=argparse.FileType("wb"), nargs="?")
-    parser.add_argument("-d", "--outDict", help="File to which to write out dictionary mapping states to move prob dist",
-                        type=argparse.FileType("wb"), nargs="?")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--modelIn", help="File containing saved trained model",
+    group = parser.add_argument_group("Source Data", "Data source for the Supervised model")
+    group.add_argument("-m", "--modelIn", help="File containing saved trained model",
                         type=argparse.FileType("rb"), nargs="?")
     group.add_argument("-p", "--pgnFiles", help="Filename(s) of pgn games to load",
                         type=argparse.FileType("r"), nargs="+")
-    group.add_argument("--pickledFile", help="Filename of picked alread-parsed pgn file to load",
-                        type = argparse.FileType("rb"), nargs="?")
     group.add_argument("-t", "--tensorData", help="Filename of file containing saved tensor data of a dataset",
                         type = argparse.FileType("rb"), nargs="?")
 
                         
 
     args = parser.parse_args()
+    if not (args.modelIn or args.pgnFiles or args.tensorData):
+        parser.error("Must have Source Data; use --modelIn, --pgnFiles, or --tensorData option")
+        exit()
 
-    supChess = SupervisedChess(savedModel = args.modelIn,
-                               pgnTensors = args.tensorData,
-                               pgnFiles   = args.pgnFiles,
-                               outTensor  = args.outTensor,
-                               outModel   = args.outModel)
-    """
-    if args.modelIn == None:
-        data = getTensorData(args)
-
-        if args.outTensor != None:
-            torch.save(data, args.outTensor)
-
-        trainedModel = trainNetworkOnData(data)
-    else:
-        trainedModel = torch.load(args.modelIn)
-
-    if args.outModel:
-        torch.save(trainedModel, args.outModel)
-    """
-    trainedModel = supChess.model
+    supChess = SupervisedChess(savedModel = args.modelIn)
+    supChess.trainModel(pgnTensors = args.tensorData,
+                       pgnFiles   = args.pgnFiles,
+                       outTensor  = args.outTensor,
+                       outModel   = args.outModel)
 
     myState = GameState.getInitialState()
     myMove  = Move("b2", "b4")
@@ -422,28 +417,6 @@ def main():
 
     bestMove = supChess.getMovePreference(myState, legalMoves)
     logging.info("Best move registered as %s" % bestMove)
-
-    """
-    moveTensor  = moveToTensor(myMove)
-    stateTensor = gameStateToTensor(myState)
-
-    moveTensor = moveTensor.float().expand((1, 132))
-    stateTensor = stateTensor.float().expand((1, 7, 8, 8))
-    singleTester = torch.utils.data.TensorDataset(stateTensor, moveTensor)
-    singleLoader = torch.utils.data.DataLoader(singleTester) 
-
-    trainedModel.eval()
-    for data, target in singleLoader:
-        output = trainedModel(data)
-        outputS = output[:, :66]
-        outputE = output[:, 66:]
-        predS = outputS.argmax(dim=1, keepdim = True)
-        predE = outputE.argmax(dim=1, keepdim = True)
-        logging.info(outputS)
-        logging.info(outputE)
-        logging.info(tensorToMove(output[0]))
-    """
-
 
 
 
